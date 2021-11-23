b0f56527e332601db2278eadf525294835e1fcf8
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-23 13:43:22 -0300

    MONGODB models

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
+
+ 
+app.use(express.json());
+app.use("/api/auth", authRoute);
