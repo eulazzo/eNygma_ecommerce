834c517d87c8292d2a42b3b3c1584a41428f9d0d
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-23 16:48:42 -0300

    nodejs JWT implementation

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
 //REGISTER
 router.post("/register", async (req, res) => {
   const newUser = new User({
@@ -35,9 +37,18 @@ router.post("/login", async (req, res) => {
     originalPassword !== req.body.password &&
       res.status(401).json("Wrong credentials!");
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
     const { password, ...others } = user._doc;
 
-    res.status(201).json(others);
+    res.status(201).json({...others, accessToken});
   } catch (err) {
     res.status(500).json(err);
   }
