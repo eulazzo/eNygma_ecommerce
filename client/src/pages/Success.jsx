7d0569251b99945b072e8e51f150a562fdf92de1
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-27 12:25:54 -0300

    react redux toolkit authentication

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -5,8 +5,6 @@ import { userRequest } from "../requestMethods";
 
 export const Success = () => {
   const location = useLocation();
-  //in Cart.jsx I sent data and cart.
-  //Please check that page for the changes.(in video it's only data)
   const data = location.state?.stripeData;
   const cart = location.state?.cart;
   const currentUser = useSelector((state) => state.user.currentUser);
@@ -49,5 +47,3 @@ export const Success = () => {
     </div>
   );
 };
-
- 
