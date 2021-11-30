16227646721c4d338a7e72272bb56ed266bd9a14
Lázaro Vanderson <eulazzo@protonmail.com>
2021-11-30 00:04:58 -0300

    handling with multiple inputs in one state

--- admin/src/pages/newProduct/NewProduct.jsx
+++ admin/src/pages/newProduct/NewProduct.jsx
@@ -1,34 +1,72 @@
+import { useState } from "react";
 import "./newProduct.css";
 
 export default function NewProduct() {
+  const [inputs, setInputs] = useState({});
+  const [file, setFile] = useState(null);
+  const [categories, setCategories] = useState([]);
+
+  const handleChange = (e) => {
+    setInputs((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleCategories = (e) => {
+    setCategories(e.target.value.split(","));
+  };
+ 
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input
+            type="file"
+            id="file"
+            onChange={(event) => setFile(event.target.value[0])}
+          />
         </div>
         <div className="addProductItem">
           <label>Title</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <input
+            name="title"
+            type="text"
+            placeholder="Apple Airpods"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Description</label>
-          <input type="text" placeholder="Descritption..." />
+          <input
+            name="desc"
+            type="text"
+            placeholder="Descritption..."
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Price</label>
-          <input type="text" placeholder="Price..." />
+          <input
+            type="number"
+            placeholder="Price..."
+            name="price"
+            onChange={handleChange}
+          />
         </div>
         <div className="addProductItem">
           <label>Categories</label>
-          <input type="text" placeholder="jeans,skirts" />
+          <input
+            type="text"
+            placeholder="jeans,skirts"
+            onChange={handleCategories}
+          />
         </div>
 
         <div className="addProductItem">
           <label>Stock</label>
-          <select>
+          <select name="inStock" onChange={handleChange}>
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
