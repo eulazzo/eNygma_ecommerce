26b3ce84dd768a9989db4e947af90c5ba2d4bd3e
Lázaro Vanderson <eulazzo@protonmail.com>
2022-01-28 20:49:00 -0300

    gifs assets for readme

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -4,7 +4,7 @@ const BASE_URL = "http://localhost:8080/api";
 
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
   .currentUser.accessToken;
- 
+
 export const publicRequest = axios.create({ baseURL: BASE_URL });
 
 export const userRequest = axios.create({
